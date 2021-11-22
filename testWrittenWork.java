66b5bceb6a06338919f9106508131174984a5f5c
Chelsearspencer <71734078+Chelsearspencer@users.noreply.github.com>
2021-11-21 20:13:07 -0500

    project 7

---
+++ testWrittenWork.java
@@ -0,0 +1,14 @@
+package cs176L_project7;
+public class testWrittenWork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+novel one = new novel("title", "name", 2000, "genre1", 1);
+shortStory two = new shortStory("title2", "name2", 2001, "genre2", 2, true);
+
+System.out.println(one);
+System.out.println();
+System.out.println(two);
+
+}}
